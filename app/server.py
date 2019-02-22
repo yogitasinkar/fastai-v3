6e1ea2d7612a5afad754bbad61713ec5150ceab5
yogita <yogita.sinkar5@gmail.com>
2019-02-23 00:56:35 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -57,4 +57,4 @@ async def analyze(request):
     return JSONResponse({'result': str(prediction)})
 
 if __name__ == '__main__':
-    if 'serve' in sys.argv: uvicorn.run(app=app, host='localhost', port=5042)
+    if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
